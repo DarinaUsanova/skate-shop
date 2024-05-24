012a3dee1ace41c1f980db9366d5fae8951fdb28
Darina Usanova <darinushkaaaa@gmail.com>
2024-05-24 15:41:35 +0200

    fix: changes for gh-pages

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,48 @@
 const { src, dest, watch, parallel, series } = require("gulp");
-
 const scss = require("gulp-sass")(require("sass"));
 const concat = require("gulp-concat");
 const browserSync = require("browser-sync").create();
 const clean = require("gulp-clean");
-const autoprefixer = require("gulp-autoprefixer");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const ghPages = require("gh-pages");
 
 function styles() {
-  return src("app/scss/style.scss")
-    .pipe(autoprefixer({ overrideBrowserslist: ["last 10 versions"] }))
-    .pipe(concat("style.min.css"))
-    .pipe(scss({ outputStyle: "compressed" }))
-    .pipe(dest("app/css"))
-    .pipe(browserSync.stream());
-}
-
-function layoutStyles() {
-  return src("layout/**.scss")
+  return src("layout/css/style.scss")
+    .pipe(scss({ outputStyle: "compressed" }).on("error", scss.logError))
+    .pipe(
+      postcss([
+        autoprefixer({
+          overrideBrowserslist: ["last 10 versions"],
+          cascade: false,
+        }),
+      ])
+    )
     .pipe(concat("style.css"))
-    .pipe(scss())
     .pipe(dest("layout/css"))
     .pipe(browserSync.stream());
 }
 
+function layoutStyles() {
+  return src("layout/**/*.scss")
+    .pipe(scss().on("error", scss.logError))
+    .pipe(concat("style.css"))
+    .pipe(dest("layout/css"))
+    .pipe(browserSync.stream());
+}
+
+function images() {
+  return src("layout/images/**/*").pipe(dest("dist/images"));
+}
+
 function watcher() {
-  watch(["app/scss/style.scss"], styles);
-  watch(["app/*.html"]).on("change", browserSync.reload);
+  watch(["layout/css/**/*.scss"], styles);
+  watch(["layout/images/**/*"], images);
+  watch(["layout/*.html"]).on("change", browserSync.reload);
 }
 
 function layoutWatcher() {
-  watch(["layout/*.scss"], layoutStyles);
+  watch(["layout/**/*.scss"], layoutStyles);
   watch(["layout/*.html"]).on("change", browserSync.reload);
 }
 
@@ -42,21 +55,26 @@ function initBrowserSync() {
 }
 
 function building() {
-  return src(["app/css/style.min.css", "app/*.html"], { base: "app" }).pipe(
-    dest("dist")
-  );
+  return src(["layout/css/style.css", "layout/*.html"], {
+    base: "layout",
+  }).pipe(dest("dist"));
 }
 
 function cleanDist() {
-  return src("dist").pipe(clean());
+  return src("dist", { allowEmpty: true }).pipe(clean());
+}
+
+function deploy(done) {
+  ghPages.publish("dist", done);
 }
 
 exports.styles = styles;
+exports.images = images;
 exports.watcher = watcher;
 exports.browserSync = initBrowserSync;
 
-exports.build = series(cleanDist, building);
-// exports.default = parallel(styles, initBrowserSync, watcher);
+exports.build = series(cleanDist, parallel(building, images));
+exports.deploy = series(cleanDist, parallel(building, images), deploy);
 exports.default = parallel(
   styles,
   layoutStyles,
