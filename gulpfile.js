cb28773ddb4d1fa069ffb9f55ec4f26257787d19
Darina Usanova <darinushkaaaa@gmail.com>
2024-05-29 19:34:40 +0200

    fix: change gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ const autoprefixer = require("autoprefixer");
 const ghPages = require("gh-pages");
 
 function styles() {
-  return src("layout/css/style.scss")
+  return src("layout/scss/**/*.scss")
     .pipe(scss({ outputStyle: "compressed" }).on("error", scss.logError))
     .pipe(
       postcss([
@@ -23,39 +23,26 @@ function styles() {
     .pipe(browserSync.stream());
 }
 
-function layoutStyles() {
-  return src("layout/**/*.scss")
-    .pipe(scss().on("error", scss.logError))
-    .pipe(concat("style.css"))
-    .pipe(dest("layout/css"))
-    .pipe(browserSync.stream());
-}
-
 function images() {
   return src("layout/images/**/*").pipe(dest("dist/images"));
 }
 
 function watcher() {
-  watch(["layout/css/**/*.scss"], styles);
+  watch(["layout/scss/**/*.scss"], styles);
   watch(["layout/images/**/*"], images);
-  watch(["layout/*.html"]).on("change", browserSync.reload);
-}
-
-function layoutWatcher() {
-  watch(["layout/**/*.scss"], layoutStyles);
   watch(["layout/*.html"]).on("change", browserSync.reload);
 }
 
 function initBrowserSync() {
   browserSync.init({
     server: {
-      baseDir: "layout/", // app/
+      baseDir: "layout/",
     },
   });
 }
 
 function building() {
-  return src(["layout/css/style.css", "layout/*.html"], {
+  return src(["layout/css/style.css", "layout/*.html", "layout/images/**/*"], {
     base: "layout",
   }).pipe(dest("dist"));
 }
@@ -72,13 +59,6 @@ exports.styles = styles;
 exports.images = images;
 exports.watcher = watcher;
 exports.browserSync = initBrowserSync;
-
-exports.build = series(cleanDist, parallel(building));
-exports.deploy = series(parallel(building), deploy);
-exports.default = parallel(
-  styles,
-  layoutStyles,
-  initBrowserSync,
-  watcher,
-  layoutWatcher
-);
+exports.build = series(cleanDist, parallel(building, images));
+exports.deploy = series(cleanDist, parallel(building, images), deploy);
+exports.default = parallel(styles, initBrowserSync, watcher);
